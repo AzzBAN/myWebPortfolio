115bd0aa31e03fb96cb89d6c75c05a5f0cee8d60
AzzBAN <azhar@ackcio.com>
2023-08-11 20:59:20 +0700

    update

--- components/Home/index.jsx
+++ components/Home/index.jsx
@@ -4,8 +4,10 @@ export default function HomePage() {
   return (
     <>
       <div className="bg-slate-700 h-[calc(100%-64px)] top-0 w-full -z-10">
-        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-          <p>Welcome to AzzBAN Web Portfolio</p>
+        <div className="flex justify-center mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 items-center h-full">
+          <div>
+            <p>Welcome to AzzBAN Web Portfolio</p>
+          </div>
         </div>
       </div>
     </>
